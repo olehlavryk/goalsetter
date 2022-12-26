47624fd8a175d19c5c60b3f4320d61b8175550f8
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-12-26 18:27:23 +0200

    feat: added front end, react in basic

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         "es2021": true
     },
     "extends": ["eslint:recommended", "prettier"],
-    "ignorePatterns": ["node_modules", ".eslintrc.js"],
+    "ignorePatterns": ["node_modules", ".eslintrc.js", "frontend"],
     "overrides": [
     ],
     "parserOptions": {
